6ebe13ccd92be4a41a53eeba387c80af5d8cedf9
Rakesh1817 <rakeshramalingam95@gmail.com>
2026-01-04 17:03:28 +0530

    create card item using foreach and display in dom and make remove button itereactive

---
+++ JS/cart.js
@@ -0,0 +1,34 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const cartContainer = document.getElementById("cart-container");
+
+if (cart.length === 0) {
+    cartContainer.innerHTML = 'cart was empty'
+}
+
+cart.forEach((game, index) => {
+
+    const card = document.createElement('div')
+    card.classList.add('game-card')
+
+    card.innerHTML = `
+    
+      <img src="${game.image}">
+    <h3>${game.name}</h3>
+    <p>₹${game.price}</p>
+    <button class="remove-btn">Remove</button>
+    `
+
+    const deleteBtn = card.querySelector('.remove-btn')
+
+    deleteBtn.addEventListener('click', () => {
+        cart.splice(index, 1)
+        localStorage.setItem('cart', JSON.stringify(cart))
+        location.reload();
+
+
+    })
+    cartContainer.appendChild(card)
+
+
+});
